fb3e71f635af1ba314a68d70ec426baa45c83802
Absolotl <andreas.peterlick@web.de>
2025-02-13 21:16:30 +0100

    fooling around

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,48 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+	const uint SCREEN_WIDTH = 1920u;
+	const uint SCREEN_HEIGHT = 1080u;
+	auto window = sf::RenderWindow(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "CMake SFML Project");
+	window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	float x = 0;
+	float y = 0;
+	float addX = 2.0F;
+	float addY = 2.0F;
 
-        window.clear();
-        window.display();
-    }
+	sf::Vector2f vector2f = sf::Vector2f(x, y);
+
+	sf::Font font("/home/Andi/.local/share/fonts/Raleway-Regular.ttf");
+	sf::Text text(font, "Hello\nWorld!", 50);
+	text.setPosition(vector2f);
+	text.setFillColor(sf::Color(0x88ffff));
+
+	sf::Drawable;
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+		x += addX;
+		y += addY;
+		if (x >= SCREEN_WIDTH - text.getGlobalBounds().size.x || x <= 0)
+		{
+			addX *= -1;
+		}
+		if (y >= SCREEN_HEIGHT - text.getGlobalBounds().size.y || y <= 0)
+		{
+			addY *= -1;
+		}
+		text.setPosition(sf::Vector2f(x, y));
+		
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
 }
